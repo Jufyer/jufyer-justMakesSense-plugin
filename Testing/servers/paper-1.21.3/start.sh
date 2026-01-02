8455b1512329076423c45a45691c0897916f999b
Jufyer <julianders@gmx.net>
2026-01-02 21:15:08 +0100

    Fix: apply updated .gitignore rules

--- Testing/servers/paper-1.21.3/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/lib/jvm/java-21-openjdk-amd64/bin/java -Xms1G -Xmx2G -jar paper-1.21.3-83.jar nogui
--- Testing/servers/paper-1.21/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/lib/jvm/java-21-openjdk-amd64/bin/java -Xms1G -Xmx2G -jar paper-1.21-130.jar nogui
